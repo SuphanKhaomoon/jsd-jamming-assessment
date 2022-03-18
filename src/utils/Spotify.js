2a6849b71736b140489493b6e94fb34cb4c73a8c
Suphan Khaomoon <jack231029@gmail.com>
2022-03-18 13:05:32 +0700

    deploy commit

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,7 +1,7 @@
 // TODO: Get Client ID from https://developer.spotify.com/dashboard/ and put it here
 const clientId = 'bce191d1f28a45ea86989eac8b692e19';
 
-const redirectUri = 'http://localhost:3000/';
+const redirectUri = 'http://suphan.surge.sh/';
 const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=token&scope=playlist-modify-public&client_id=${clientId}&redirect_uri=${redirectUri}`;
 let accessToken = undefined;
 let expiresIn = undefined;
